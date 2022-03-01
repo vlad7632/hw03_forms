fe95adf716b12a082d8dcbca07e0802a66bb2e9f
Владислав Воробьев <vlad7632@gmail.com>
2022-03-01 23:48:40 +0300

    Спринт 4 финал

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,11 @@
-from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Group, Post, User
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, redirect, render
+
 from .forms import PostForm
+from .models import Group, Post, User
 
 
-@login_required
 def index(request):
     posts = Post.objects.all()
     paginator = Paginator(posts, 10)
@@ -32,17 +32,17 @@ def group_posts(request, slug):
 
 
 def profile(request, username):
-    user = get_object_or_404(User, username=username)
-    posts = user.posts.all()
+    author = get_object_or_404(User, username=username)
+    posts = author.posts.all()
     post_count = posts.count()
     paginator = Paginator(posts, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
-        'user': user,
         'posts': posts,
         'post_count': post_count,
         'page_obj': page_obj,
+        'author': author,
     }
     return render(request, 'posts/profile.html', context)
 
